cf80c555cf89311fe7d37dab8a6ee26abdeee26e
Jaspreet Chahal <jaspreetchahal.org@gmail.com>
2012-07-23 16:11:32 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ jctextcopyprotector
 ===================
 
 This plugin will give some protection to your content being getting copied. There are many options that gives you granular control over plugin functionality.
+
+
+<h2>Options</h2>
+<ul>
+	<li><strong>blockRightClick:true|false</strong>,
+This option if set to true will block context menu to appear</li>
+	<li><strong>blockDocTextSelection:true|false,</strong>
+This option if set to true will disable text selection using mouse</li>
+	<li><strong>useCSS:true,
+</strong>This option if set to true will use CSS to block text selection on your document</li>
+	<li><strong>blockPageSave:true,
+</strong>This option if set to true will block keyboard event such as Ctrl+A, Ctrl+S , Ctrl+C</li>
+	<li><strong>alertUser:false,
+</strong>This option if set to true will show an alert message when a blocked event is detected.</li>
+	<li><strong>alertMessage:''
+</strong>Custom alert message</li>
+</ul>
+
+Please visit http://jaspreetchahal.org to read more
---
+++ jccopyblock.js
@@ -0,0 +1,50 @@
+jQuery.fn.jccopyblock = function(settings) {
+          settings = jQuery.extend({
+            blockRightClick:true,
+            blockDocTextSelection:true,
+            useCSS:true,
+            blockPageSave:true,
+            alertUser:false,
+            alertMessage:'Sorry! Content copy is not allowed?',
+            callback:function(){}
+          },settings);
+          if(settings.blockRightClick) {
+            jQuery(document).contextmenu(function(evt){
+                if(settings.alertUser) {
+                  alert(settings.alertMessage);
+                }
+                evt.preventDefault();        
+                return false;
+            });
+          }
+          if(settings.blockDocTextSelection && !settings.useCSS) {
+            jQuery(document)[0].onselectstart = function(evt) { 
+                if(settings.alertUser) {
+                  alert(settings.alertMessage);
+                }
+                evt.preventDefault();
+                return false;
+            };
+          }
+          else if(settings.blockDocTextSelection && settings.useCSS) {
+                jQuery('html,body').css({
+                   '-moz-user-select':'none',
+                   '-webkit-user-select':'none',
+                   'user-select':'none',
+                   '-ms-user-select':'none'
+               });
+          } 
+          if(settings.blockPageSave) {
+            var ctrl = false;
+            jQuery(window).keydown(function(e) {
+                if (e.keyCode == 17) ctrl = true;
+            }).keyup(function(e) {
+                if (e.keyCode == 17) ctrl = false;
+            });
+            jQuery(window).keydown(function(e) {
+                if (ctrl && (e.keyCode == 83 || e.keyCode == 115 || e.keyCode == 97 || e.keyCode == 65 || e.keyCode == 67 || e.keyCode == 99)) { // blocks CTRL c (copy)+a(select all)+s(save)
+                  return false;
+                }
+            });           
+          }
+        };
